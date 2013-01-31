75668578c9de81fa83c6dc2e881a9fea3409fe98
michael <michael@montgomer.(none)>
2013-01-31 16:23:39 +1100

    Added Ore Measure header file

---
+++ ore_measure.h
@@ -0,0 +1,14 @@
+#ifndef ORE_MEASURE_H
+#define ORE_MEASURE_H
+
+typedef struct _ore_measure *OreMeasure;
+struct _ore_measure {
+	Measurement ore;
+	Measurement mineral;
+	Measurement grade;
+} _ore_measure
+
+OreMeasure newOreMeasure();
+
+#endif
+
