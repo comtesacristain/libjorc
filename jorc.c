aade118e57057a4a8201f872513eabab2b449921
Michael Sexton <michael.sexton@ga.go.au>
2013-02-14 17:25:52 +1100

    Moved headers around, added text to README

--- jorc.c
+++ jorc.c
@@ -1,7 +1,7 @@
-#include "measurement.h"
+
 #include "jorc.h"
-#include <stdio.h>
 
 Jorc newJorc() {
-	return Jorc;
+	Jorc x;
+	return x;
 }
--- ore_measure.h
+++ ore_measure.h
@@ -1,6 +1,8 @@
 #ifndef ORE_MEASURE_H
 #define ORE_MEASURE_H
 
+#include "measurement.h"
+
 typedef struct _ore_measure *OreMeasure;
 struct _ore_measure {
 	Measurement ore;
